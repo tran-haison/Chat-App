7524f4bd186cf7aafcfd5b5fa75195dea36f6fab
Tran Hai Son <haison250199@gmail.com>
2021-05-17 13:53:20 +0700

    Nothing much

--- Chat-Client/app/src/main/java/com/example/chat_client/socket/Client.java
+++ Chat-Client/app/src/main/java/com/example/chat_client/socket/Client.java
@@ -6,7 +6,6 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
@@ -15,23 +14,18 @@ import java.net.Socket;
 public class Client {
 
     private static Client client = null;
-    private ClientThread clientThread;
+
+    private Socket socket;
+    private BufferedReader input;
+    private boolean isSocketRunning = false;
     private final MutableLiveData<String> responseMessage = new MutableLiveData<>();
 
     public static final String TAG = "ClientSocket";
-    public static final String IP_ADDRESS = "192.168.1.14";
-    public static final int PORT = 54000;
+    public static final String IP_ADDRESS = "192.168.1.16";
+    public static final int PORT = 8080;
 
     private Client() {
-        try {
-            // Connect to server
-            Log.d(TAG, "Connecting to server...");
-            clientThread = new ClientThread();
-            Thread thread = new Thread(clientThread);
-            thread.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        connect();
     }
 
     public synchronized static Client getInstance() {
@@ -41,91 +35,78 @@ public class Client {
         return client;
     }
 
+    private void connect() {
+        Thread thread = new Thread(() -> {
+            try {
+                // Establish connection
+                Log.d(TAG, "Connecting to server...");
+                InetAddress serverAddress = InetAddress.getByName(IP_ADDRESS);
+                socket = new Socket(serverAddress, PORT);
+                Log.d(TAG, "Connected to server!");
+                Log.d(TAG, "Server address: " + serverAddress.toString());
+
+                // Listening for response from server
+                try {
+                    input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    listeningForResponse();
+                } catch (Exception e) {
+                    Log.d(TAG, "Error getting input stream: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                Log.d(TAG, "Error unknown: " + e.getMessage());
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
     public void sendMessage(final String message) {
-        clientThread.sendMessage(message);
+        Thread thread = new Thread(() -> {
+            try {
+                if (socket != null) {
+                    // Send message to server
+                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                    printWriter.write(message);
+                    printWriter.flush();
+                    Log.d(TAG, message);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
+    private void listeningForResponse() {
+        isSocketRunning = true;
+        Log.d(TAG, "Listening for server response...");
+        while (isSocketRunning) {
+            try {
+                String message = input.readLine();
+                if (message != null) {
+                    Log.d(TAG, "Server response: " + message);
+                    //responseMessage.setValue(message);
+                } else {
+                    Log.d(TAG, "Server response nothing!");
+                }
+            } catch (Exception e) {
+                Log.d(TAG, "Error receiving from server: " + e.getMessage());
+                e.printStackTrace();
+                break;
+            }
+        }
     }
 
     public void closeSocket() {
-        clientThread.closeSocket();
+        try {
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public LiveData<String> responseMessageLiveData() {
         return responseMessage;
-    }
-
-    private class ClientThread implements Runnable {
-
-        private Socket socket;
-        private BufferedReader input;
-        private boolean isSocketRunning = false;
-
-        @Override
-        public void run() {
-            isSocketRunning = true;
-            try {
-                // Establish connection
-                InetAddress serverAddress = InetAddress.getByName(IP_ADDRESS);
-                socket = new Socket(serverAddress, PORT);
-                Log.d(TAG, "Connected to server...");
-                Log.d(TAG, "Server address: " + serverAddress.toString());
-
-                // Listen for upcoming message
-                receiveMessage();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        void sendMessage(final String message) {
-            Thread thread = new Thread(() -> {
-                try {
-                    if (socket != null) {
-                        // Send message to server
-                        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-                        printWriter.write(message);
-                        printWriter.flush();
-                        Log.d(TAG, message);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-            thread.start();
-        }
-
-        void receiveMessage() {
-            // Get input stream from socket
-            try {
-                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            } catch (Exception e) {
-                Log.d(TAG, "Error getting input stream: " + e.getMessage());
-                e.printStackTrace();
-            }
-
-            Log.d(TAG, "Listening for server response...");
-            while (isSocketRunning) {
-                try {
-                    String message = input.readLine();
-                    if (message != null) {
-                        responseMessage.setValue(message);
-                        Log.d(TAG, "Server response: " + message);
-                    } else {
-                        Log.d(TAG, "Server response nothing!");
-                    }
-                } catch (Exception e) {
-                    Log.d(TAG, "Error receiving from server: " + e.getMessage());
-                    e.printStackTrace();
-                    break;
-                }
-            }
-        }
-
-        void closeSocket() {
-            try {
-                socket.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
